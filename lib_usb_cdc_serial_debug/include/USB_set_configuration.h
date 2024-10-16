8101ae9b3467c8f8a62f9e4fbd8827568f75c526
serialcomms <serialcomms@outlook.com>
2024-10-16 14:54:06 +0100

    Delete lib_usb_cdc_serial_debug directory

--- lib_usb_cdc_serial_debug/include/USB_set_configuration.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2024 Serialcomms (GitHub).
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-extern bool HID_CONFIGURATION_SET;
-
-void usb_set_configuration(uint8_t configuration_number);
