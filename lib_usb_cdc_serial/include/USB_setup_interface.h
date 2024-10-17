6908e6fa292e77eb5bcfbe2efb29b87af0b69be0
serialcomms <serialcomms@outlook.com>
2024-10-17 10:24:55 +0100

    Add files via upload

---
+++ lib_usb_cdc_serial/include/USB_setup_interface.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 Serialcomms (GitHub).
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+void usb_setup_interface();
+
+static inline void setup_interface_respond_to_host();
+static inline void setup_interface_request_to_pico();
+static inline void setup_interface_unknown();
